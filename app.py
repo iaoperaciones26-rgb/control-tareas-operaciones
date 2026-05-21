eeb81c5e9556eba2a23607cf422b2cbfbb4b9368
iaoperaciones26-rgb <iaoperaciones26@gmail.com>
2026-05-21 15:32:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 import streamlit as st
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-st.title("🚀 Control Tareas Operaciones")
+st.title("📊 Control Tareas Operaciones")
 
-st.write("Si ves esto, la app está funcionando")
+# Leer credenciales desde secrets
+creds_dict = st.secrets["gcp_service_account"]
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+client = gspread.authorize(creds)
+
+# Abrir Google Sheets
+sheet = client.open("BD_TAREAS_OPERACIONES").sheet1
+
+# Leer datos
+data = sheet.get_all_records()
+
+df = pd.DataFrame(data)
+
+st.write(df)
