d088eb4ea5b68eee66ad25e71de524cbccd05554
iaoperaciones26-rgb <iaoperaciones26@gmail.com>
2026-07-14 11:43:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if not st.session_state["login_ok"]:
 
     if st.button("Ingresar"):
 
-        if password == "OPE2026":
+        if password == "JEAN":
             st.session_state["login_ok"] = True
             st.rerun()
         else:
